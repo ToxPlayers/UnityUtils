05f53d255f4da5b8d6ab434e77b9c7e38c4836ad
Tox <toxplayers@gmail.com>
2025-04-14 22:43:14 +0300

    Update ValueCalculator.cs

--- Runtime/ValueCalculators/ValueCalculator.cs
+++ Runtime/ValueCalculators/ValueCalculator.cs
@@ -5,7 +5,7 @@ using TriInspector;
 abstract public class ValueCalculator<T> where T : struct
 { 
 
-    public interface IModifier  {  public T Modify(ref T value); } 
+    public interface IModifier  {  public void Modify(ref T value); } 
     [ShowInInspector, ReadOnly] protected readonly HashSet<IModifier> _modifiers = new();
     public void RegisterModifier(IModifier mod) => _modifiers.Add(mod);  
     public void UnregisterModifier(IModifier mod) => _modifiers.Remove(mod);
