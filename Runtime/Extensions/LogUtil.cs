5364b0afc0f0401c039c85f61cc4e614d98c6d8d
Tox <toxplayers@gmail.com>
2025-09-03 16:44:57 +0300

    Update LogUtil.cs

--- Runtime/Extensions/LogUtil.cs
+++ Runtime/Extensions/LogUtil.cs
@@ -21,7 +21,22 @@ static public class LogUtil
 	static public string ColorRedSoft(string str) => Color(str, RedSoft);
 	static public string Color(string str, UColor color)
 		=> ColorStart(color) + str + ColorEnd;
-	  
+	public static string KiloFormat(this int num)
+    {
+        if (num >= 100000000)
+            return (num / 1000000).ToString("#,0M");
+
+        if (num >= 10000000)
+            return (num / 1000000).ToString("0.#") + "M";
+
+        if (num >= 100000)
+            return (num / 1000).ToString("#,0K");
+
+        if (num >= 10000)
+            return (num / 1000).ToString("0.#") + "K";
+
+        return num.ToString("#,0");
+    }
     static public string GetStackLog(int skipFrames = 1, int maxFrameCount = 10 , bool useHyperLink = true)
 	{  
 		var frames = new StackTrace(skipFrames , true).GetFrames();
