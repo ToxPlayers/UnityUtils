a765234fe8462f3275483c94c7b2f22b724d5169
Tox <toxplayers@gmail.com>
2025-10-03 15:33:25 +0300

    singletons conditional instance

--- Runtime/Singeltons/SingletonExtensions.cs
+++ Runtime/Singeltons/SingletonExtensions.cs
@@ -5,130 +5,135 @@ using System;
 using System.Reflection;
 using System.Linq;
 using NUnit.Framework;
-static public class SingletonExtensions
+namespace SingletonBehaviors
 {
-#if UNITY_EDITOR
-    [UnityEditor.MenuItem("Tools/Singletons/Remove All Singeltons Instances")]
-    [UnityEditor.InitializeOnEnterPlayMode] 
-    static void RemoveAllInstances()
-	{ 
-        var log = "";
-        int removedCount = 0; 
-        var singTypes = CExtensions.GetDerivingTypes(typeof(SingletonMono<>));
-        foreach (var singType in singTypes)
-        {
-            try
-            {
-                var prop = singType.BaseType?.GetRuntimeProperty("Instance");
-                if (prop == null)
-                    log += singType + " Doesnt have instance prop";
-                else
-                {
-                    log += "Removed Singleton For " + singType + '\n';
-                    prop.SetValue(null, null);
-                    removedCount++;
-                }
-            }
-            catch (Exception ex) { Debug.LogException(ex); }
-        }  
-        if(removedCount > 0)
-            Debug.Log($"Removed {removedCount} singletons\n{log}");  
-    }
-    /*[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]*/
-    [UnityEditor.MenuItem("Tools/Singletons/Force All Singeltons Instances")]
-    static void ForceAllBeforeSceneLoad() => ForceAll();
-#endif
-
-    public static void ForceAll()
+    static public class SingletonExtensions
     {
-        string log = ""; 
-        var singTypes = CExtensions.GetDerivingTypes(typeof(SingletonMono<>)); 
-        if(singTypes.Count() == 0)
-            return;
+    #if UNITY_EDITOR
+        [UnityEditor.MenuItem("Tools/Singletons/Remove All Singeltons Instances")]
+        [UnityEditor.InitializeOnEnterPlayMode] 
+        static void RemoveAllInstances()
+	    { 
+            var log = "";
+            int removedCount = 0; 
+            var singTypes = CExtensions.GetDerivingTypes(typeof(SingletonMono<>));
+            foreach (var singType in singTypes)
+            {
+                try
+                {
+                    var prop = singType.BaseType?.GetRuntimeProperty("Instance");
+                    if (prop == null)
+                        log += singType + " Doesnt have instance prop";
+                    else
+                    {
+                        log += "Removed Singleton For " + singType + '\n';
+                        prop.SetValue(null, null);
+                        removedCount++;
+                    }
+                }
+                catch (Exception ex) { Debug.LogException(ex); }
+            }  
+            if(removedCount > 0)
+                Debug.Log($"Removed {removedCount} singletons\n{log}");  
+        }
+        /*[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]*/
+        [UnityEditor.MenuItem("Tools/Singletons/Force All Singeltons Instances")]
+        static void ForceAllBeforeSceneLoad() => ForceAll();
+    #endif
+
+        public static void ForceAll()
+        {
+            string log = ""; 
+            var singTypes = CExtensions.GetDerivingTypes(typeof(SingletonMono<>)); 
+            if(singTypes.Count() == 0)
+                return;
             
-        var propertyFlags = BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy;
-        List<Type> instanceForced = new();
-        List<Type> alreadyHasInstance = new();
-        List<Type> notFound = new();
-        List<Type> errorTypes = new();
-		foreach (Type type in singTypes)
-        {
+            var propertyFlags = BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy;
+            List<Type> instanceForced = new();
+            List<Type> alreadyHasInstance = new();
+            List<Type> notFound = new();
+            List<Type> errorTypes = new();
+		    foreach (Type type in singTypes)
+            {
            
-            try
-            {
-                var hasInstance = (bool)type.GetProperty(nameof(SingletonMono<MonoBehaviour>.HasInstance), propertyFlags).GetValue(null, null);
-                if (hasInstance)
+                try
                 {
-                    alreadyHasInstance.Add(type);
-                    log += $"{type.Name} \n";
-                    continue;
+                    var hasInstancPropName = nameof(SingletonMono<SingletonMono>.HasInstance);
+                    var hasInstance = (bool)type.GetProperty(hasInstancPropName, propertyFlags).GetValue(null, null);
+                    if (hasInstance)
+                    {
+                        alreadyHasInstance.Add(type);
+                        log += $"{type.Name} \n";
+                        continue;
+                    }
+                    var findAndForceRootGosName = nameof(SingletonMono<SingletonMono>.FindAndForceInstanceAtRootGOs);
+                    var method = type.GetMethod(findAndForceRootGosName, propertyFlags);
+                    var singObj = (UnityEngine.Object) method.Invoke(null, null); 
+                    if (singObj)
+                    {
+                        type.GetMethod(nameof(SingletonMono<SingletonMono>.ForceInstance)).Invoke(singObj, null);
+                        instanceForced.Add(type);
+                    }
+                    else
+                        notFound.Add(type);
                 }
-                var method = type.GetMethod(nameof(SingletonMono<MonoBehaviour>.FindAndForceInstanceAtRootGOs), propertyFlags);
-                var singObj = (UnityEngine.Object) method.Invoke(null, null); 
-                if (singObj)
+                catch (Exception ex)
                 {
-                    type.GetMethod(nameof(SingletonMono<MonoBehaviour>.ForceInstance)).Invoke(singObj, null);
-                    instanceForced.Add(type);
+                    Debug.LogException(ex);
+                    errorTypes.Add(type);
                 }
-                else
-                    notFound.Add(type);
             }
-            catch (Exception ex)
-            {
-                Debug.LogException(ex);
-                errorTypes.Add(type);
-            }
+
+            AddStringifyList("Forced instances:", instanceForced, ref log);
+            AddStringifyList("Allready has instance:", alreadyHasInstance, ref log);
+            AddStringifyList("Singelton not found:", notFound, ref log);
+            AddStringifyList( LogUtil.ColorRed("Errors:"),errorTypes, ref log);
+
+            log = $"Forced {instanceForced.Count}/{singTypes.Count()} instances:\n{log}\n";
+		    Debug.Log(log); 
         }
-
-        AddStringifyList("Forced instances:", instanceForced, ref log);
-        AddStringifyList("Allready has instance:", alreadyHasInstance, ref log);
-        AddStringifyList("Singelton not found:", notFound, ref log);
-        AddStringifyList( LogUtil.ColorRed("Errors:"),errorTypes, ref log);
-
-        log = $"Forced {instanceForced.Count}/{singTypes.Count()} instances:\n{log}\n";
-		Debug.Log(log); 
-    }
     
-    static string GetColoredStringName(Type t) => LogUtil.Color(t.Name, new Color(0, 0.6f, 0)) + ".cs";
-    static void AddStringifyList(string header, List<Type> types, ref string log)
-    {
-        log += '\n';
-        if (types.Count > 0)
-            log += header + '\n';
-
-        foreach (var nf in types)
-            log += GetColoredStringName(nf) + '\n';
-    }
-
-    static bool IsSubclassOfGeneric(Type type, Type baseType)
-    {
-        if (type == null || baseType == null || type == baseType)
-            return false;
-
-        if (baseType.IsGenericType == false)
+        static string GetColoredStringName(Type t) => LogUtil.Color(t.Name, new Color(0, 0.6f, 0)) + ".cs";
+        static void AddStringifyList(string header, List<Type> types, ref string log)
         {
-            if (type.IsGenericType == false)
-                return type.IsSubclassOf(baseType);
-        }
-        else
-        {
-            baseType = baseType.GetGenericTypeDefinition();
+            log += '\n';
+            if (types.Count > 0)
+                log += header + '\n';
+
+            foreach (var nf in types)
+                log += GetColoredStringName(nf) + '\n';
         }
 
-        type = type.BaseType;
-        Type objectType = typeof(object);
-
-        while (type != objectType && type != null)
+        static bool IsSubclassOfGeneric(Type type, Type baseType)
         {
-            Type curentType = type.IsGenericType ?
-                type.GetGenericTypeDefinition() : type;
-            if (curentType == baseType)
-                return true;
+            if (type == null || baseType == null || type == baseType)
+                return false;
+
+            if (baseType.IsGenericType == false)
+            {
+                if (type.IsGenericType == false)
+                    return type.IsSubclassOf(baseType);
+            }
+            else
+            {
+                baseType = baseType.GetGenericTypeDefinition();
+            }
 
             type = type.BaseType;
+            Type objectType = typeof(object);
+
+            while (type != objectType && type != null)
+            {
+                Type curentType = type.IsGenericType ?
+                    type.GetGenericTypeDefinition() : type;
+                if (curentType == baseType)
+                    return true;
+
+                type = type.BaseType;
+            }
+
+            return false;
         }
 
-        return false;
     }
-
 }
--- Runtime/Singeltons/SingletonMono.cs
+++ Runtime/Singeltons/SingletonMono.cs
@@ -4,88 +4,98 @@ using Sirenix.OdinInspector;
 #else
 using TriInspector;
 #endif
-
-abstract public class SingletonMono<T> : MonoBehaviour  where T : MonoBehaviour
+namespace SingletonBehaviors
 {
-    private static T _instance = null;
-    public static bool HasInstance => _instance; 
-    public static Transform TF => _instance ? _instance.transform : null;
-    public static T Instance
-    { 
-        private set
-        {
-            if (_instance && value == _instance)
-                return;
-
-            if(HasInstance)
-                (_instance as SingletonMono<T>).OnInstanceRemoved(); 
-
-            _instance = value;
-             
-            if (_instance != null)
-            {
-                var instanceSing = _instance as SingletonMono<T>; 
-                instanceSing.OnInstanceSet();
-            } 
-        }
-        get
-        {
-            if (!_instance)
-            {
-                FindAndForceInstanceAtRootGOs();
-                return _instance ??=  FindFirstObjectByType<T>(FindObjectsInactive.Include);
-            }
-            return _instance;
-        }
-    }  
-
-    static public T FindAndForceInstanceAtRootGOs()
-	{
-        if (_instance)
-            return _instance;
-
-        if ( UnityExtensions.TryGetRootComp<T>(out var rootT) ) 
-            return Instance = rootT; 
-
-        var inst = FindFirstObjectByType<T>();
-        if (inst)
-            (inst as SingletonMono<T>).ForceInstance();
-        return inst;
+    public abstract class SingletonMono : MonoBehaviour
+    {
+        public virtual bool CanBeInstance { get; } = true;
+        /// <summary>
+        /// Called when this object is set to Instance of <typeparamref name="T"></typeparamref>
+        /// </summary>
+        internal virtual void OnInstanceSet() { }
+        /// <summary>
+        /// Called when this object is no longer the Instance object.
+        /// </summary>
+        internal virtual void OnInstanceRemoved() { }
     }
-     
-    public bool IsInstance { get => _instance && this == _instance; }
-#if UNITY_EDITOR
-    string EDITOR_BTN_NAME => IsInstance ? "Instance Class" : "Force Instance";
-    [PropertyOrder(100), DisableIf(nameof(IsInstance)), Button("$" + nameof(EDITOR_BTN_NAME))]
-#endif
-    public void ForceInstance() => Instance = this as T;   
 
-	protected virtual void Awake()
-	{  
-#if UNITY_EDITOR
-        var prefabStatus = UnityEditor.PrefabUtility.GetPrefabInstanceStatus(gameObject);
-        if(prefabStatus == UnityEditor.PrefabInstanceStatus.NotAPrefab)
-		{
-#endif
-            T thisT = this as T;
-            if (!HasInstance) 
-                Instance = thisT;  
-#if UNITY_EDITOR
+    abstract public class SingletonMono<T> : SingletonMono where T : SingletonMono 
+    {
+        private static T _instance = null;
+        public static bool HasInstance => _instance; 
+        public static Transform TF => Instance ? Instance.transform : null; 
+        public static T Instance
+        { 
+            private set
+            {
+                if (_instance && value == _instance)
+                    return;
+
+                if (value && !value.CanBeInstance)
+                    return;
+
+                if(HasInstance)
+                    _instance.OnInstanceRemoved(); 
+
+                _instance = value;
+             
+                if (_instance != null)
+                {
+                    var instanceSing = _instance as SingletonMono<T>; 
+                    instanceSing.OnInstanceSet();
+                } 
+            }
+            get
+            {
+                if (!_instance)
+                {
+                    FindAndForceInstanceAtRootGOs();
+                    _instance ??=  FindFirstObjectByType<T>(FindObjectsInactive.Include);
+                    if (!_instance)
+                        Debug.LogError("Could not find instance of type " + typeof(T).Name);
+                }
+                return _instance;
+            }
+        }  
+
+        static public T FindAndForceInstanceAtRootGOs()
+	    {
+            if (_instance)
+                return _instance;
+
+            if ( UnityExtensions.TryGetRootComp<T>(out var rootT) ) 
+                return Instance = rootT; 
+
+            var inst = FindFirstObjectByType<T>();
+            if (inst)
+                (inst as SingletonMono<T>).ForceInstance();
+            return inst;
         }
-#endif 
-    } 
-    protected virtual void OnDestroy()
-	{
-		if (_instance == this)
-            Instance = null;
-	} 
-
-    /// <summary>
-    /// Called when this object is set to Instance of <typeparamref name="T"></typeparamref>
-    /// </summary>
-    protected virtual void OnInstanceSet() {}
-    /// <summary>
-    /// Called when this object is no longer the Instance object.
-    /// </summary>
-    protected virtual void OnInstanceRemoved() {} 
+     
+        public bool IsInstance { get => _instance && this == _instance; }
+    #if UNITY_EDITOR
+        string EDITOR_BTN_NAME => IsInstance ? "Instance Class" : "Force Instance";
+        [PropertyOrder(100), DisableIf(nameof(IsInstance)), Button("$" + nameof(EDITOR_BTN_NAME))]
+    #endif
+        public void ForceInstance() => Instance = this as T;
+	    protected virtual void Awake()
+	    {  
+    #if UNITY_EDITOR
+            var prefabStatus = UnityEditor.PrefabUtility.GetPrefabInstanceStatus(gameObject);
+            if(prefabStatus == UnityEditor.PrefabInstanceStatus.NotAPrefab)
+		    {
+    #endif
+                T thisT = this as T;
+                if (!HasInstance) 
+                    Instance = thisT;  
+    #if UNITY_EDITOR
+            }
+    #endif 
+        } 
+        protected virtual void OnDestroy()
+	    {
+		    if (_instance == this)
+                Instance = null;
+	    } 
+    }
 }
