39ccc0abf9763dddbfcb241c3ea743dcd9d54352
Tox <67845762+ToxPlayers@users.noreply.github.com>
2026-09-25 01:01:16 +0300

    Add preprocessor directives for Unity version check

--- Runtime/Singeltons/AwakeScriptableSingletons.cs
+++ Runtime/Singeltons/AwakeScriptableSingletons.cs
@@ -1,8 +1,15 @@
 using UnityEditor;
 using UnityEngine;
+#if UNITY_6000_5_OR_NEWER
+using Unity.Scripting.LifecycleManagement;
+#endif 
 class AwakeScriptableSingletons
 {
+#if UNITY_6000_5_OR_NEWER
+    [OnCodeInitializing]
+#else  
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+#endif 
     static void ForceAllInstances()
     {
         foreach (var inst in Resources.LoadAll<ScriptableSingleton>(ScriptableSingleton.SingletonsResFolder))
