044da4f2c2afade380a7c5d6074d2a856545ff78
Tox <67845762+ToxPlayers@users.noreply.github.com>
2026-09-25 01:04:08 +0300

    Change AwakeScriptableSingletons to static partial class

--- Runtime/Singeltons/AwakeScriptableSingletons.cs
+++ Runtime/Singeltons/AwakeScriptableSingletons.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 #if UNITY_6000_5_OR_NEWER
 using Unity.Scripting.LifecycleManagement;
 #endif 
-class AwakeScriptableSingletons
+static partial class AwakeScriptableSingletons
 {
 #if UNITY_6000_5_OR_NEWER
     [OnCodeInitializing]
